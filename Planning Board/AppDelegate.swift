1c42abecf76a182ed1d84fd1f73b8b1ad21922e0
Callindsey <Cal@calebs-macbook-pro.local>
2017-02-11 22:56:10 -0500

    Version for Pastor Jordan that doesn't crash on start up

--- Planning Board/AppDelegate.swift
+++ Planning Board/AppDelegate.swift
@@ -19,8 +19,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         FIRApp.configure()
         // Override point for customization after application launch.
-        
-        let userLogginStatus = UserDefaults.standard.value(forKey: "logged_in") as! Bool
+        var userLogginStatus = Bool()
+        if UserDefaults.standard.value(forKey: "logged_in") != nil {
+            userLogginStatus = UserDefaults.standard.value(forKey: "logged_in") as! Bool
+        }
         
         if (userLogginStatus) {
             self.window?.rootViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "signedInView")

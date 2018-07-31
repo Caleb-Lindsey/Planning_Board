c0690624230ade86bee443d5c08e2bd6250cfc3a
Cal Lindsey <boldferociousdogwarrior@gmail.com>
2018-07-31 00:05:12 -0400

    idk

--- Pods/Target Support Files/Pods-Planning BoardTests/Pods-Planning BoardTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Planning_BoardTests : NSObject
-@end
-@implementation PodsDummy_Pods_Planning_BoardTests
-@end
--- Pods/Target Support Files/Pods-Planning BoardUITests/Pods-Planning BoardUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Planning_BoardUITests : NSObject
-@end
-@implementation PodsDummy_Pods_Planning_BoardUITests
-@end
